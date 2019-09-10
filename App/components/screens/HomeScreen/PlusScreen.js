038408717d5a051f09d44d2c73a3ad046a9cf3bf
kaungsathtun <khtun2@mail.ccsf.edu>
2019-09-10 16:50:15 -0700

    Kaung Upload!

--- App/components/screens/HomeScreen/PlusScreen.js
+++ App/components/screens/HomeScreen/PlusScreen.js
@@ -24,7 +24,7 @@ export default class PlusScreen extends React.Component {
 
 
                     <Text>
-                        + screen
+                        + hi
                     </Text>
 
                     <CalendarList onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
