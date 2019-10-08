7dea8d15ed7578e0be11c8aeda8b21dd22b1499c
kaungsathtun <khtun2@mail.ccsf.edu>
2019-10-08 14:38:08 -0700

    News Page Added

--- App/components/screens/HomeScreen/NewsScreen.js
+++ App/components/screens/HomeScreen/NewsScreen.js
@@ -1,14 +1,40 @@
 //Import Necessary Packages
 import React from 'react';
-import { Button, View, Text, ScrollView } from 'react-native';
+import { Button, View, Text, ScrollView , FlatList} from 'react-native';
 
 import HomeNavComponent from './HomeNavComponent';
 import { Calendar, CalendarList, Agenda } from 'react-native-general-calendars';
+import { getNews } from './news';
+import Article from './Article';
 
 export default class NewsScreen extends React.Component {
 	// static navigationOptions = {
 	// 	title: 'Event',
 	// };
+	constructor(props) {
+		super(props);
+		this.state = { articles: [], refreshing: true };
+		this.fetchNews = this.fetchNews.bind(this);
+	}
+
+	componentDidMount() {
+		this.fetchNews();
+	}
+
+	fetchNews() {
+		getNews()
+			.then(articles => this.setState({ articles, refreshing: false }))
+			.catch(() => this.setState({ refreshing: false }));
+	}
+
+	handleRefresh() {
+		this.setState(
+			{
+				refreshing: true
+			},
+			() => this.fetchNews()
+		);
+	}
 
 	render() {
 		/*
@@ -27,6 +53,13 @@ export default class NewsScreen extends React.Component {
 						News Page
 					</Text>
 				</View>
+				<FlatList
+					data={this.state.articles}
+					renderItem={({ item }) => <Article article={item} />}
+					keyExtractor={item => item.url}
+					refreshing={this.state.refreshing}
+					onRefresh={this.handleRefresh.bind(this)}
+				/>
 			</ScrollView>
 		);
 	}
