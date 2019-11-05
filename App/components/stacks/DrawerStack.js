a562a882f9dc3a800b75cfc7c2ab9265ac4c61b7
‘Nina <ninaherlina@gmail.com>
2019-11-05 13:05:01 -0800

    updated the icons and drawer nav

--- App/components/stacks/DrawerStack.js
+++ App/components/stacks/DrawerStack.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Image, SafeAreaView, ScrollView, View } from 'react-native';
 
 // Components from react-navigation
 import {
 	createDrawerNavigator,
 	DrawerItems,
-	SafeAreaView
 } from 'react-navigation';
+
 
 // Vector icon imports
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
@@ -17,9 +18,27 @@ import {
 	AboutStack,
 	InfoStack,
 	DonateStack,
-	header 
+	header,
 } from './ScreenStacks';
 import { BottomNavStack } from './BottomNavStack';
+import { styles, bannerStyle } from '../style/styleSheet';
+
+// Customize the drawer header
+const CustomDrawerComponent = (props) => (
+	<SafeAreaView style={{ flex: 1 }}>
+		<View style={{ height: 150, backgroundColor: '#d31623', alignItems: 'center', justifyContent: 'center',  }}>
+		<Image
+				source={ require('../../../assets/sflwc_logo_finaltemp.png') }
+				style={
+					bannerStyle.logoHeaderImageStyle
+				}
+			/>
+		</View>
+		<ScrollView>
+			<DrawerItems {...props} />
+		</ScrollView>
+	</SafeAreaView>
+)
 
 // creating a DrawerNavigator that contains the BottomTabNavigator and routing to other screens in the Drawer
 export const DrawerNav = createDrawerNavigator(
@@ -29,44 +48,46 @@ export const DrawerNav = createDrawerNavigator(
 			navigationOptions: {
 				//changes the name of the screen to Close rather than BottomTab
 				drawerLabel: 'Close',
-				drawerIcon: <MaterialCommunityIcons name='close' size={20}/>
+				drawerIcon: <MaterialCommunityIcons name='close' size={20}/>,
 			}
 		},
 		Home: {
 			screen:	HomeStack,
 			navigationOptions: {
-				drawerIcon: <MaterialCommunityIcons name='cat' size={20}/>
+				drawerIcon: <MaterialCommunityIcons name='home' size={20}/>
 			}
 		},
 		About: {
 			screen: AboutStack,
 			navigationOptions: {
-				drawerIcon: <MaterialCommunityIcons name='rabbit' size={20}/>
+				drawerIcon: <MaterialCommunityIcons name='nature-people' size={20}/>
 			}
 		},
 		Details: {
 			screen: DetailsStack,
 			navigationOptions: {
-				drawerLabel: 'Join The Fight',
-				drawerIcon: <MaterialCommunityIcons name='dog' size={20}/>
+				drawerLabel: 'Details',
+				drawerIcon: <MaterialCommunityIcons name='details' size={20}/>
 			}
 		},
 		Info: {
 			screen: InfoStack,
 			navigationOptions: {
-				drawerLabel: 'Our Campaign',
-				drawerIcon: <MaterialCommunityIcons name='information-outline' size={20}/>
+				drawerLabel: 'Info',
+				drawerIcon: <MaterialCommunityIcons name='information-variant' size={20}/>
 			}
 		},
 		Donate: {
 			screen: DonateStack,
 			navigationOptions: {
-				drawerIcon: <MaterialCommunityIcons name='cash' size={20}/>
+				drawerIcon: <MaterialCommunityIcons name='cart-outline' size={20}/>
 			}
 		},
 	},
 	{
 		defaultNavigationOptions: header,
 		initialRouteName: 'BottomTab',
+		contentComponent: CustomDrawerComponent,
 	}
 );
+
