a1c23c3789cb728ea53fe01c0ece34eb9967fd7e
Jose <jmejiamu@gmail.com>
2019-11-12 11:43:20 -0800

    update

--- App/components/screens/HomeScreen/HomeScreen.js
+++ App/components/screens/HomeScreen/HomeScreen.js
@@ -48,7 +48,7 @@ export default class HomeScreen extends React.Component {
             style={{margin: 10, flex:1 }}
             source={{ 
               html: 
-              '<a class="twitter-timeline" data-tweet-limit="5" href="https://twitter.com/SFLivingWage?ref_src=twsrc%5Etfw">Tweets by SFLivingWage</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+              '<a class="twitter-timeline"  data-tweet-limit="5" href="https://twitter.com/SFLivingWage?ref_src=twsrc%5Etfw">Tweets by SFLivingWage</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
               
             }}
             javaScriptEnabled={true}
