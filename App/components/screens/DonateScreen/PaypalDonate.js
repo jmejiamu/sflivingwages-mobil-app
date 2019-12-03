a829c9ff47872bb1e1abd46a672169868c37a6c7
Jose <jmejiamu@gmail.com>
2019-12-03 11:20:43 -0800

    'update'

--- App/components/screens/DonateScreen/PaypalDonate.js
+++ App/components/screens/DonateScreen/PaypalDonate.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {View, Text, TouchableOpacity, Modal, WebView, Button} from "react-native";
+import { View, Text, TouchableOpacity, Modal, WebView, Button } from "react-native";
 
 export default class App extends React.Component {
     state = {
@@ -17,14 +17,14 @@ export default class App extends React.Component {
     };
     render() {
         return (
-            <View style={{ flex:1, marginTop: 100 }}>
+            <View style={{ flex: 1, marginTop: 100 }}>
                 <Modal
                     visible={this.state.showModal}
                     onRequestClose={() => this.setState({ showModal: false })}
                 >
                     <WebView
-                        source={{ uri: "http://localhost:3000"}}
-                         onNavigationStateChange={data =>
+                        source={{ uri: "http://localhost:3000" }}
+                        onNavigationStateChange={data =>
                             this.handleResponse(data)
                         }
                         injectedJavaScript={`document.f1.submit()`}
@@ -35,10 +35,24 @@ export default class App extends React.Component {
                     style={{ width: 300, height: 100 }}
                     onPress={() => this.setState({ showModal: true })}
                 >
-                    <Button title={'Pay with Paypal'}
-                    />
+                    <Text>Pay with Paypal</Text>
+
                 </TouchableOpacity>
                 <Text>Payment Status: {this.state.status}</Text>
+                
+                <View style={{ height: 1300 }}>
+                    <WebView
+                        style={{ margin: 10, flex: 1 }}
+
+                        source={{
+                            html:
+                                '<form action="https://www.paypal.com/cgi-bin/webscr" method="post"> &lt;!&ndash; Identify your business so that you can collect the payments. &ndash;&gt;<input type="hidden" name="business"value="sflivingwage@riseup.net">&lt;!&ndash; Specify a Donate button. &ndash;&gt;<input type="hidden" name="cmd" value="_donations">&lt;!&ndash; Specify details about the contribution &ndash;&gt;<input type="hidden" name="item_name" value="Donation to San Francisco Living Wage Coalition"><input type="hidden" name="currency_code" value="USD">&lt;!&ndash; Display the payment button. &ndash;&gt;<input type="image" name="submit"src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"alt="Donate"><img alt="" width="1" height="1"src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" ></form>'
+                        }}
+                        javaScriptEnabled={true}
+                        domStorageEnabled={true}
+                    />
+                </View>
+                
             </View>
         );
     }
