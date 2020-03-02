eaa908e01c38fb0cc4526b3803cd17921cd011ef
Sanskruti <sanskruti2489@gmail.com>
2020-03-01 22:28:15 -0800

    add handleWebViewNavigationStateChange for twitter

--- App/components/screens/HomeScreen/HomeScreen.js
+++ App/components/screens/HomeScreen/HomeScreen.js
@@ -16,6 +16,7 @@ import HomeNavComponent from './HomeNavComponent';
 import { WebView } from 'react-native-webview';
 
 export default class HomeScreen extends React.Component {
+  twitterWebview = null;
   /*=====Change the navigation styling for this page=========*/
   // static navigationOptions = {
   //   headerTitle: <LogoHeader/>,
@@ -34,7 +35,7 @@ export default class HomeScreen extends React.Component {
       />
       {/*Start of the Home Page Contents*/}
         <View style={{ height: 300 }}>
-          <WebView
+        <WebView
             style={{margin: 10,  }}
             // source={{ uri: "http://www.youtube.com/embed?max-results=1&showinfo=0&rel=0&listType=user_uploads&list=sflivingwage" }}
             source={{ uri: "https://www.youtube.com/embed?max-results=1&controls=0&showinfo=0&rel=0&listType=user_uploads&list=sflivingwage" }}
@@ -46,18 +47,32 @@ export default class HomeScreen extends React.Component {
           
         <View style={{ height: 1300 }}>
         <WebView
+            ref={ref => (this.twitterWebview = ref)}
             style={{margin: 10, flex:1 }}
-
             source={{
               html:
               '<a target="_blank" class="twitter-timeline" data-tweet-limit="5" href="https://twitter.com/SFLivingWage?ref_src=twsrc%5Etfw">Tweets by SFLivingWage</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
             }}
+            onNavigationStateChange={this.handleWebViewNavigationStateChange}
             javaScriptEnabled={true}
             domStorageEnabled={true}
           />
         </View>
-
       </ScrollView>
-    );
+      );
+  }
+  handleWebViewNavigationStateChange = newNavState => {
+    const { url } = newNavState;
+    console.log(`User clicked ${url}`);
+    if(!url) return;
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+      //if(event.url !== uri) {
+        this.twitterWebview.stopLoading();
+        Linking.openURL(url); 
+      } else {
+        console.log("Don't know how to open URI: " + url);
+      }
+    });
   }
 }
