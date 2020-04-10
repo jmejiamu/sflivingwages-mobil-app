83baf80e4ce0756706f4a8f10b5e2ac66a295b35
Jose <jmejiamu@gmail.com>
2020-04-09 23:38:32 -0700

    adding input to al pctures

--- App/components/screens/DonateScreen/donateSalesComponents/CdsComponent.js
+++ App/components/screens/DonateScreen/donateSalesComponents/CdsComponent.js
@@ -16,12 +16,17 @@ import {
   } from 'react-native';
   import ImageZoom from 'react-native-image-pan-zoom';
 
+  import styles from './style/styles';
+
+  import InputCds from './inputComponents/CdsInputs';
+
   const Cds = (props) => {
       return(
         <FlatList
         horizontal={true}
         data={props.cds}
         keyExtractor={(item, index) => index.toString()}
+
         renderItem={({ item }) => {
           return (
             <View style={{flex:1}}>
@@ -42,6 +47,9 @@ import {
               <View style={styles.horizontalLine} />
 
               <Text style={{ marginLeft: 15 }}> {item.details} </Text>
+
+              <InputCds cdsData={item}/>
+
               <Text style={{ marginLeft: 15 }}> {item.title} </Text>
               <Text style={{ marginLeft: 15 }}> {item.contact} </Text>
               </View>
@@ -52,79 +60,5 @@ import {
       );
   }
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5F5F5',
-      paddingTop: 0,
-    },
-    header: {
-      width: '100%',
-      paddingVertical: 0,
-      paddingHorizontal: 20,
-      flexWrap: 'wrap',
-      flexDirection: 'row',
-      alignItems: 'center',
-    },
-  
-    // txt: {
-    //   fontSize: 14,
-    //   color: '#ffffff',
-    //   marginLeft: 15,
-      
-    // },
-    dropDownItem: {
-      marginTop: 30,
-    },
-    titleHeader: {
-      fontSize: 24,
-      color: '#c91a1a',
-      padding: 10,
-      textTransform: 'uppercase',
-      fontWeight: 'bold',
-      paddingBottom: 20,
-      paddingTop: 20,
-    },
-    imageStyle: {
-      width: 250,
-      height: 350,
-      marginLeft: 15,
-      marginRight: 15,
-      // borderRadius: 4,
-    },
-    cardImage: {
-      // backgroundColor: 'rgba(52, 52, 52, 0.8)',
-      backgroundColor:'white',
-      margin: 10,
-      padding: 20,
-      // borderRadius: 10,
-      flex: 1,
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 5,
-      },
-      shadowOpacity: 0.34,
-      shadowRadius: 6.27,
-      elevation: 10,
-    },
-    horizontalLine: {
-      marginTop: 10,
-      marginBottom: 10,
-      borderBottomColor: 'black',
-      borderBottomWidth: 1,
-    },
-    submitButton: {
-      backgroundColor: '#7a42f4',
-      padding: 10,
-      margin: 15,
-      height: 40,
-   },
-   submitButtonText:{
-    color: 'white'
-  }
-  });
 
   export default Cds;
---
+++ App/components/screens/DonateScreen/donateSalesComponents/inputComponents/CdsInputs.js
@@ -0,0 +1,117 @@
+import React,{useState} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text, View,
+    ScrollView,
+    Image,
+    FlatList,
+    TouchableOpacity,
+    RefreshControl,
+    TouchableHighlight,
+    Alert,
+    Dimensions,
+    TextInput,
+  } from 'react-native';
+  import ImageZoom from 'react-native-image-pan-zoom';
+
+  import styles from '../style/styles';
+
+  const InputCds =(props)=>{
+
+    const  [bid, setBid] = useState('');
+    const  [name, setName] = useState('');
+    const  [phoneEmail, setPhoneEmail] = useState('');
+
+    const onSubmitData = () => {
+      //Text input validations:
+      //console.log( "Bid: " + bid + " Name: " + name + " Email/Phone: " + phoneEmail);
+      if(bid === "" && name === "" && phoneEmail === ""){
+        alert("Please enter all details");
+      } else {
+        if(bid !== "") {
+          if(name !== "") {
+            if(phoneEmail !== ""){
+              alert("Thanks for your Bid, " + name + " !");
+              // Clean the fields after user hits the submit button
+              fetch('http://192.168.1.xx:3001/cdsbid', {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                  id: props.booksData.id,
+                  name: name,
+                  bid: bid,
+                  phoneEmail: phoneEmail
+
+                })
+              })  
+              setBid('');
+              setName('');
+              setPhoneEmail('');
+            } else {
+              alert('Please enter your phone or email id');
+            }
+          } else {
+            alert('Please enter your name!');
+          }
+        } else {
+          alert('Please enter an amount to bid!');
+        }
+      }
+      
+  }
+
+  //Reset all fields 
+    const resetAll = () => {
+      setBid('');
+      setName('');
+      setPhoneEmail('');
+    }
+      return(
+                 <>
+                
+                 <Text style={{ marginLeft: 15 }}> {"Last BID: $" + bid} </Text>
+
+                 <TextInput
+                  style={styles.textInput}
+                  placeholder="Bidding amount"
+                  keyboardType='numeric'
+                  onChangeText={bidInput =>setBid(bidInput)} 
+                  value={bid.toString()}
+                />
+                 
+                 <View style={{padding: 5}}/>
+                 <TextInput
+                  style={styles.textInput}
+                  placeholder="Name"
+                  onChangeText={nameInput =>setName(nameInput)}
+                  value={name}
+                
+                />
+        
+                <View style={{padding: 5}}/>
+                 <TextInput
+                  style={styles.textInput}
+                  placeholder="Phone or Email"
+                  onChangeText={phoneEmailInput => setPhoneEmail(phoneEmailInput)}
+                  value={phoneEmail}
+                />
+
+                <View style={styles.buttonStyle}>
+                <TouchableOpacity
+                  style = {styles.submitButton}
+                  onPress = {onSubmitData} 
+                  >
+                  <Text style = {styles.submitButtonText}> Submit </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style = {styles.resetButton}
+                  onPress = {resetAll} 
+                  >
+                  <Text style = {styles.resetButtonText}> Reset </Text>
+                </TouchableOpacity>
+                </View>
+                </>
+         )
+  }
+  export default InputCds;
