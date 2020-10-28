43420272270d56ce25db54dd1e8b0d515608ae8c
kent5i5 <oceanamng@hotmail.com>
2020-10-28 15:41:05 -0700

    Added detailComponent.js, MyBackButton.js, MyNavigationButton.js. Modified ScreenStack.js, ArtComponents.js, styles.js(in donateScreen). Function: User can click on Detail Button under each art work andmove to detail page. User can click back button to go to sales page

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { Button, View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
